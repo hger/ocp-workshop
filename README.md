410b61faed176ebc085f7815803dcd4450cf76a7
Håkan Hagenrud <hakan@redhat.com>
2022-06-15 12:59:12 +0200

    added skeleton files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ocp-workshop
+
+Point to this git project from openshift developer console and import directly from git.
---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+ <title>Openshift workshop</title>
+ </head>
+ <body>
+ <p><img src="https://www.redhat.com/profiles/rh/themes/redhatdotcom/img/logo.svg" alt="Red Hat logo" width="250">
+ <font size="6">
+<!-- <br><//?php echo 'App Version: 2.0.'; ?> -->
+ <br><?php echo 'Pod IP: '; ?><?php echo $_SERVER['SERVER_ADDR']; ?>
+ <br><?php echo ' Current php Version: ' . phpversion(); ?>
+ </font>
+ </p>
+ </body>
+</html>
+
